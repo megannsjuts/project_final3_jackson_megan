2c30902bd0d39a78ab74474942f6518f2a1dfbed
Megan Jackson <meganjackson@Megans-MacBook-Pro-2.local>
2017-04-19 09:38:58 -0700

    add hover images and custom jQuery code for image hover

--- js/main.js
+++ js/main.js
@@ -34,3 +34,57 @@ $('.owl-carousel').owlCarousel({
 		}
 	}
 });
+
+$('#chips img').hover(function () {
+	$(this).attr('src', 'img/chipshover.png');
+}, function () {
+	$(this).attr('src', 'img/chips.jpg');
+});
+
+$('#fries img').hover(function () {
+	$(this).attr('src', 'img/frieshover.png');
+}, function () {
+	$(this).attr('src', 'img/fries.jpg');
+});
+
+$('#salad img').hover(function () {
+	$(this).attr('src', 'img/saladhover.png');
+}, function () {
+	$(this).attr('src', 'img/salad.jpg');
+});
+
+$('#turkey img').hover(function () {
+	$(this).attr('src', 'img/turkeyhover.png');
+}, function () {
+	$(this).attr('src', 'img/turkey.jpg');
+});
+
+$('#ham img').hover(function () {
+	$(this).attr('src', 'img/hamhover.png');
+}, function () {
+	$(this).attr('src', 'img/ham.jpg');
+});
+
+$('#wrap img').hover(function () {
+	$(this).attr('src', 'img/wraphover.png');
+}, function () {
+	$(this).attr('src', 'img/wrap.jpg');
+});
+
+$('#cookies img').hover(function () {
+	$(this).attr('src', 'img/cookieshover.png');
+}, function () {
+	$(this).attr('src', 'img/cookies.jpg');
+});
+
+$('#brownies img').hover(function () {
+	$(this).attr('src', 'img/brownieshover.png');
+}, function () {
+	$(this).attr('src', 'img/brownies.jpg');
+});
+
+$('#cake img').hover(function () {
+	$(this).attr('src', 'img/cakehover.png');
+}, function () {
+	$(this).attr('src', 'img/cake.jpg');
+});
